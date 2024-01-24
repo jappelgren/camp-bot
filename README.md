fdab6ef70af83b1cad80cc997dc4c2af324ed025
jappelgren <justinappelgren@gmail.com>
2024-01-24 10:37:00 -0600

    added extra step to launch browser and login a minute before time set as CRON_EXPRESSION to speed up process

--- README.md
+++ README.md
@@ -13,11 +13,10 @@ A Puppeteer bot made for a friend to snipe camping spot reservartions on [recrea
    - `CHECK_IN_DATE` - mm/dd/yyy ex. `'05/15/2024'` 
    - `CHECK_OUT_DATE` - mm/dd/yyy ex. `'05/17/2024'` 
    - `CRON_EXPRESSION` - How often you want the application to check for camp sites.  I was told by my friend that camp sites are released at 9 AM central time, which would be `'0 9 * * *'`.  If you want to run it at a different interval, feel free. [Cron helper app](https://crontab.guru/) 
-   - `TIME_ZONE` - The time zone you will be running the bot in. eg. `'US/Central'`. [Complete list of valid timezones](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) 
 4. ```
    npm run start
    ``` 
-   - The application will run at an interval set by the `CRON_EXPRESSION` environment variable.  If the bot succeeds in adding a campsite to your cart the window will stay open and you will have 15 minutes to complete the transaction.  If there are no sites available it will shut the browser window and try again when your cron expression tells it to. 
+   - The application will run at an interval set by the `CRON_EXPRESSION` environment variable. The browser will open and login you in a minute before the time set in `CRON_EXPRESSION`.  It will attempt to add a camp site to your cart at the time provided in `CRON_EXPRESSION`.  If the bot succeeds in adding a campsite to your cart the window will stay open and you will have 15 minutes to complete the transaction.  If there are no sites available it will shut the browser window and try again when your cron expression tells it to. 
    - If you book a site make sure to shut down the app so it doesn't try to keep adding sites to your cart. 
 
     [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
--- src/main.js
+++ src/main.js
@@ -1,109 +1,125 @@
 import puppeteer from "puppeteer";
-import { CronJob } from "cron";
+import * as schedule from "node-schedule";
 import dotenv from 'dotenv';
 
 dotenv.config();
 const cronExpression = process.env.CRON_EXPRESSION;
-const timeZone = process.env.TIME_ZONE;
 
-const reserveCampSite = () => {
+const createBrowser = async () => {
   const campGroundId = process.env.CAMP_GROUND_ID;
   const userName = process.env.USER_NAME;
   const pwd = process.env.PASSWORD;
+
+  const browser = await puppeteer.launch({ headless: false, });
+  const incognito = await browser.createIncognitoBrowserContext();
+  const page = await incognito.newPage();
+  await page.setViewport({ width: 1920, height: 1080 })
+
+  await page.goto(`https://www.recreation.gov/camping/campgrounds/${campGroundId}`);
+  // Login
+  const loginBtn = '#ga-global-nav-log-in-link';
+  await page.waitForSelector(loginBtn);
+  await page.click(loginBtn);
+
+  const emailInput = '#email';
+  const pwdInput = '#rec-acct-sign-in-password';
+  const loginSubmitBtn = '.rec-acct-sign-in-btn'
+
+  await page.waitForSelector(emailInput);
+  await page.type(emailInput, userName);
+  await page.type(pwdInput, pwd);
+  await page.click(loginSubmitBtn);
+
+  schedule.scheduleJob('reserveCampsite', cronExpression, () => reserveCampSite(browser, page));
+}
+
+const reserveCampSite = async (browser, page) => {
   const checkInDate = process.env.CHECK_IN_DATE;
   const checkOutDate = process.env.CHECK_OUT_DATE;
 
+  await page.reload()
 
-  (async () => {
-    const browser = await puppeteer.launch({ headless: false, });
-    const incognito = await browser.createIncognitoBrowserContext();
-    const page = await incognito.newPage();
-    page.setViewport({ height: 1080, width: 1920 })
-    await page.goto(`https://www.recreation.gov/camping/campgrounds/${campGroundId}`);
+  // Filter sites
+  const listViewButton = '.campsite-list-button-toggle'
+  const checkInFilterInput = '#startDate';
+  const checkOutFilterInput = '#endDate';
 
-    // Login
-    const loginBtn = '#ga-global-nav-log-in-link';
-    await page.waitForSelector(loginBtn);
-    await page.click(loginBtn);
+  await page.waitForSelector(listViewButton);
+  await page.click(listViewButton);
 
-    const emailInput = '#email';
-    const pwdInput = '#rec-acct-sign-in-password';
-    const loginSubmitBtn = '.rec-acct-sign-in-btn'
+  await page.waitForSelector(checkInFilterInput);
+  await page.type(checkInFilterInput, checkInDate);
+  await page.type(checkOutFilterInput, checkOutDate);
 
-    await page.waitForSelector(emailInput);
-    await page.type(emailInput, userName);
-    await page.type(pwdInput, pwd);
-    await page.click(loginSubmitBtn);
+  //Find available camp sites
+  const paginationList = '.rec-pagination'
 
-    // Filter sites
-    const listViewButton = '.campsite-list-button-toggle'
-    const checkInFilterInput = '#startDate';
-    const checkOutFilterInput = '#endDate';
+  await page.waitForSelector(paginationList);
+  const pages = await page.$$(`${paginationList} > li`);
+  const pageNumbers = [];
 
-    await page.waitForSelector(checkInFilterInput);
-    await page.click(listViewButton);
-    await page.click(listViewButton);
-    await page.type(checkInFilterInput, checkInDate);
-    await page.type(checkOutFilterInput, checkOutDate);
-
-    //Find available camp sites
-    const paginationList = '.rec-pagination'
-
-    await page.waitForSelector(paginationList);
-    const pages = await page.$$(`${paginationList} > li`);
-    const pageNumbers = [];
-
-    for (const [i, el] of pages.entries()) {
-      const pgNum = await el.$eval('li > button', el => el.textContent);
-      if (i > 0 && i < pages.length - 1) {
-        pageNumbers.push(+pgNum);
-      }
+  for (const [i, el] of pages.entries()) {
+    const pgNum = await el.$eval('li > button', el => el.textContent);
+    if (i > 0 && i < pages.length - 1) {
+      pageNumbers.push(+pgNum);
     }
+  }
 
-    const nextPageButton = 'button[aria-label="Go to next page"]';
+  const nextPageButton = 'button[aria-label="Go to next page"]';
 
-    let curPage = pageNumbers[0];
-    const lastPage = pageNumbers[pageNumbers.length - 1];
-    let addedToCart = false;
+  let curPage = pageNumbers[0];
+  const lastPage = pageNumbers[pageNumbers.length - 1];
+  let addedToCart = false;
 
-    while (curPage < lastPage) {
-      // Create array of available sites on current page
-      const availableSites = await page.$$('.campsite-search-card-column')
-      for (const site of availableSites) {
-        if (site.$eval('span', el => el.textContent) !== 'Accessible') {
-          const addToCartButton = (await site.$eval('button', el => el.className)).includes('list-map-book-now-button-tracker')
-            ? await site.$('.list-map-book-now-button-tracker')
-            : ''
-          if (addToCartButton) {
-            await addToCartButton.click();
-            await page.waitForSelector('.rec-order-detail-page-title')
-            addedToCart = true;
-            console.log('Camp site added to cart.')
-            break;
-          }
+  while (curPage < lastPage) {
+    // Create array of available sites on current page
+    const availableSites = await page.$$('.campsite-search-card-column')
+    for (const site of availableSites) {
+      if (site.$eval('span', el => el.textContent) !== 'Accessible') {
+        const addToCartButton = (await site.$eval('button', el => el.className)).includes('list-map-book-now-button-tracker')
+          ? await site.$('.list-map-book-now-button-tracker')
+          : ''
+        if (addToCartButton) {
+          await addToCartButton.click();
+          await page.waitForSelector('.rec-order-detail-page-title')
+          addedToCart = true;
+          console.log('Camp site added to cart.')
+          break;
         }
       }
-
-      if (addedToCart) {
-        break;
-      }
-
-      await page.click(nextPageButton)
-      curPage++
-      await page.waitForSelector(`button[aria-label="Current page, page ${curPage}"]`);
     }
 
-    if (!addedToCart) {
-      console.log('No camp sites found today, will try again tomorrow.');
-      await browser.close();
-      return;
+    if (addedToCart) {
+      break;
     }
 
-    await browser.disconnect();
-    console.log('If this site works for you make sure to stop the application AFTER checking out to avoid adding additional campsites to your cart tomorrow.');
-    console.log('Stopping the application before you checkout will close the browser. But the site should still be in your cart if you reopen the page in a regular browser.')
-  })();
+    await page.click(nextPageButton)
+    curPage++
+    await page.waitForSelector(`button[aria-label="Current page, page ${curPage}"]`);
+  }
+
+  if (!addedToCart) {
+    console.log('No camp sites found today, will try again tomorrow.');
+    await browser.close();
+    return;
+  }
+
+  await browser.disconnect();
+  console.log('If this site works for you make sure to stop the application AFTER checking out to avoid adding additional campsites to your cart tomorrow.');
+  console.log('Stopping the application before you checkout will close the browser. But the site should still be in your cart if you reopen the page in a regular browser.');
 }
 
 console.log('Application started')
-const _job = new CronJob(cronExpression, reserveCampSite, true, timeZone);
+
+const subtractMinuteFromCron = (cronExpression) => {
+  const splitExpression = cronExpression.split(' ')
+  if (+splitExpression[0] === 0) {
+    splitExpression[0] = 59
+    splitExpression[1] = +splitExpression[1] - 1
+  } else {
+    splitExpression[0] = +splitExpression[0] - 1
+  }
+  return `${splitExpression[0]} ${splitExpression[1]} ${splitExpression[2]} ${splitExpression[3]} ${splitExpression[4]}`
+}
+
+schedule.scheduleJob('openBrowser', subtractMinuteFromCron(cronExpression), createBrowser);
